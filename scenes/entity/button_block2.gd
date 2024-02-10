f33d2f41adc3d2336b1b66e86a302004af22a4cd
cruncheon <38970996+cruncheon@users.noreply.github.com>
2024-02-10 13:13:39 +0900

    First commit

---
+++ scenes/entity/button_block2.gd
@@ -0,0 +1,49 @@
+@tool
+
+extends "res://addons/level_block/level_block_node.gd"
+
+## Button for ButtonBlock2
+## Has light switch function
+
+func interact():
+	if west_face == 47:
+		self.west_face = 12
+		
+		## Light Up Hallway
+		$"../../Lights/OmniLight2".light_color = Color("ffdabf")
+		$"../../Lights/OmniLight2".omni_range = 6
+		
+		$"../../Lights/OmniLight5".light_color = Color("ffdabf")
+		$"../../Lights/OmniLight5".omni_range = 6
+		
+		$"../../Lights/OmniLight6".light_color = Color("ffdabf")
+		$"../../Lights/OmniLight6".omni_range = 6
+		
+		$"../../Lights/OmniLight9".light_color = Color("ffdabf")
+		$"../../Lights/OmniLight9".omni_range = 6
+		
+		$"../../Lights/OmniLight10".light_color = Color("ffdabf")
+		$"../../Lights/OmniLight10".omni_range = 6
+		
+	elif west_face == 12:
+		self.west_face = 47
+		
+		## Revert Hallway lights
+		$"../../Lights/OmniLight2".light_color = Color("ff4947")
+		$"../../Lights/OmniLight2".omni_range = 8
+		
+		$"../../Lights/OmniLight5".light_color = Color("ff4947")
+		$"../../Lights/OmniLight5".omni_range = 3
+		
+		$"../../Lights/OmniLight6".light_color = Color("ff4947")
+		$"../../Lights/OmniLight6".omni_range = 3
+		
+		$"../../Lights/OmniLight9".light_color = Color("ff4947")
+		$"../../Lights/OmniLight9".omni_range = 3
+		
+		$"../../Lights/OmniLight10".light_color = Color("ff4947")
+		$"../../Lights/OmniLight10".omni_range = 3
+		
+		
+		
+
